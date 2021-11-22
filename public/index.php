2acbf1eb8c1f2ae695f170f38631f254025c8bde
NaveenNirmal <naveennirmalparanawithana@gmail.com>
2021-11-22 11:30:54 +0530

    create log and register view complete

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ministry of Health</title>
-    <link rel="stylesheet" href="./Resources/CSS/props.css">
     <link rel="stylesheet" href="./Resources/CSS/index.css">
 </head>
 
--- public/register.php
+++ public/register.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ministry of Health</title>
-    <link rel="stylesheet" href="./Resources/CSS/props.css">
     <link rel="stylesheet" href="./Resources/CSS/register.css">
 </head>
 
@@ -28,9 +27,25 @@
         </div>
         <div class="logdtls">
             <form action="../DB/log.php" method="POST">
-                <h2>Sign In</h2>
-                <input type="text" name="Username" placeholder="User Name" required>
-                <input type="password" name="Pass" placeholder="Password" required>
+                <h2>Registretion Form</h2>
+                <div>
+                    <div class="left">
+                        <input type="text" name="Username" placeholder="Name" required>
+                        <input type="text" name="Username" placeholder="Gender" required>
+                    </div>
+
+                    <div>
+                        <input type="text" name="Username" placeholder="Age" required>
+                        <input type="text" name="district" placeholder="District" required>
+                    </div>
+
+                    <div class="right">
+                        <input type="text" name="Username" placeholder="email" required>
+                        <input type="password" name="Pass" placeholder="Password" required>
+
+                    </div>
+                </div>
+
                 <input id="logbtn" type="submit" value="Login">
                 <p>Don't have account ? </p>
                 <a id="reglink" href="./register.php">Click here to register</a>
